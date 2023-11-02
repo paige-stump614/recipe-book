dc9dc6b252146486be5aa9d2221488b2058e12ec
paige-stump614 <paige.stump614@proton.me>
2023-11-02 08:55:36 -0400

    implement dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false;
 
-  constructor() { }
+  @HostListener('click') toggleOpen(){
+    this.isOpen = !this.isOpen;
+  }
 
 }
