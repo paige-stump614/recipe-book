3e68ab03b7b9b40bf8f70c077249e16bd3fe0ad5
paige-stump614 <paige.stump614@proton.me>
2023-11-03 14:36:44 -0400

    add ingredients from a recipe to the shopping list with service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,26 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>();
-    
-    private recipes: Recipe[] = [new Recipe('A test recipe', 'description placeholder for recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s'), 
-    new Recipe('A second Recipe!', 'description placeholder for recipe number 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s'), 
-    new Recipe('Holy Crap! aThird Recipe', 'description placeholder for recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s'), 
-    new Recipe('four is so many recipes', 'description placeholder for recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s')];
+    private recipes: Recipe[] = [new Recipe('A test recipe', 'description placeholder for recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s', [new Ingredient('eggs', 5), new Ingredient('dough', 2)]), 
+    new Recipe('A second Recipe!', 'description placeholder for recipe number 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s', [new Ingredient('eggs', 5), new Ingredient('dough', 2)]), 
+    new Recipe('Holy Crap! aThird Recipe', 'description placeholder for recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s', [new Ingredient('eggs', 5), new Ingredient('dough', 2)]), 
+    new Recipe('four is so many recipes', 'description placeholder for recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwX4qeXQQ11jNCEYtQbx9xiU8AYbXm3l1OSotK-VhhvQ&s', [new Ingredient('eggs', 5), new Ingredient('dough', 2)])];
+
+    constructor(private shoppingListService: ShoppingListService) {
+    }
 
     getRecipes() {
         //slice like this returns a copy of the values stored in the variable instead of a reference to the variable
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 
     
